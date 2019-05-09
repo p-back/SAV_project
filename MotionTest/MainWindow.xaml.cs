a005411978d186f13479118bfa026dcb34cdadeb
Nico <nico.teringl@students.fh-hagenberg.at>
2019-05-09 12:34:10 +0200

    Initial Commit --> HELLO ELIASSON!

---
+++ MotionTest/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MotionTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Create the serial port with basic settings
+        private SerialPort port = new SerialPort("COM14", 115200, Parity.None, 8, StopBits.One);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Attach a method to be called when there
+            // is data waiting in the port's buffer
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+
+            // Begin communications
+            port.Open();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            //Console.WriteLine(port.ReadExisting());
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                output.Text += port.ReadExisting();
+            }));
+            
+        }
+    }
+}
