387a73d3ae7254ed0d72e19c152997d864846f28
95Elias <s1810567003@students.fh-hagenberg.at>
2019-05-26 16:37:37 +0200

    added choose level page

---
+++ ball_in_a_maze/Levels/LevelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ball_in_a_maze
+{
+    class LevelBase
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public GameField.GameElements[,] Level { get; set; }
+    }
+}
